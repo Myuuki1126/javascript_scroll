38307c4296615d70c96532c204603f7d0bb7e03b
Myuuki1126 <109127643+Myuuki1126@users.noreply.github.com>
2023-10-30 15:04:32 +0900

    Add files via upload

--- script.js
+++ script.js
@@ -1,24 +1,24 @@
 // JavaScript
 //実行する機能
-const show = (entries,obs) => {
+const show = (entries, obs) => {
     entries.forEach((entry) => {
-        if (entry.isIntersecting){
-        const keyframes = {
-          opacity: [0, 1],
-          translate: ['200px 0',0]
-        }
-        entry.target.animate(keyframes,600);
+        if (entry.isIntersecting) {
+            const keyframes = {
+                opacity: [0, 1],
+                translate: ['-400px 100px', 0]
+            }
+            entry.target.animate(keyframes, 1000);
 
-        //一度表示されたら止める
-      obs.unobserve(entry.target);
+            //一度表示されたら止める
+            obs.unobserve(entry.target);
         }
     });
-  }
-  
-  const observer = new IntersectionObserver(show);
-  
-  //監視対象
-  const images = document.querySelectorAll('.img');
-  images.forEach(img =>{
+}
+
+const observer = new IntersectionObserver(show);
+
+//監視対象
+const images = document.querySelectorAll('.img');
+images.forEach(img => {
     observer.observe(img);
-  });
+});
